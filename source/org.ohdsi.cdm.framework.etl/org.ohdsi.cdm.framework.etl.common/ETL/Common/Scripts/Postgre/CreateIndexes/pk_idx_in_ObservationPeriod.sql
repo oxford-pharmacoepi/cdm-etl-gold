a8e161f363aafade784e8a5bb942273d6703100d
Teen Man <114593559+teenwyman@users.noreply.github.com>
2023-05-31 12:21:32 +0100

    Added idx_person_id

--- source/org.ohdsi.cdm.framework.etl/org.ohdsi.cdm.framework.etl.common/ETL/Common/Scripts/Postgre/CreateIndexes/pk_idx_in_ObservationPeriod.sql
+++ source/org.ohdsi.cdm.framework.etl/org.ohdsi.cdm.framework.etl.common/ETL/Common/Scripts/Postgre/CreateIndexes/pk_idx_in_ObservationPeriod.sql
@@ -1,3 +1,3 @@
 ALTER TABLE {sc}.observation_period ADD CONSTRAINT xpk_observation_period PRIMARY KEY (observation_period_id);
 CREATE INDEX IF NOT EXISTS idx_observation_period_id ON {sc}.observation_period (person_id ASC);
-CLUSTER {sc}.observation_period  USING idx_observation_period_id;
+CLUSTER {sc}.observation_period USING idx_observation_period_id;
--- source/org.ohdsi.cdm.framework.etl/org.ohdsi.cdm.framework.etl.common/ETL/Common/Scripts/Postgre/CreateIndexes/pk_idx_in_Person.sql
+++ source/org.ohdsi.cdm.framework.etl/org.ohdsi.cdm.framework.etl.common/ETL/Common/Scripts/Postgre/CreateIndexes/pk_idx_in_Person.sql
@@ -1,2 +1,3 @@
 ALTER TABLE {sc}.person ADD CONSTRAINT xpk_person PRIMARY KEY (person_id);
-CLUSTER {sc}.person USING xpk_person;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_person_id ON {sc}.person (person_id ASC);
+CLUSTER {sc}.person USING idx_person_id;
